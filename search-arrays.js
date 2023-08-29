273d52f6313893c103992b3e6746ed03f23c63fe
unknown <o.adrian.rodrigues@gmail.com>
2023-08-29 18:07:21 -0300

    search-arrays up

--- search-arrays.js
+++ search-arrays.js
@@ -2,6 +2,11 @@ const study = ["Jão", "July", "Ana", "Caio"]
 const mid = [10, 8, 7, 5, 9]
 const studyMid = [study, mid]
 
+//Nomeando cada arrays de dentro do array principal com um nome especifico
+const [students, midway] = studyMid;
+
+console.log(students, midway)
+
 function showNameScore(aluno) {
     if (studyMid[0].includes(aluno)) {
         console.log(`${aluno} está cadastrado! E Sua media escolar é: ${studyMid[1][studyMid[0].indexOf(aluno)]}`)
