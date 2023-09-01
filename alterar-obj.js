4c0922fcf55476b689e96be3bcfc05d61a115afd
unknown <o.adrian.rodrigues@gmail.com>
2023-09-01 17:01:17 -0300

    spread operator for update obj

--- alterar-obj.js
+++ alterar-obj.js
@@ -8,10 +8,33 @@ people.telefone = "11 000000000";
 console.log(people.telefone);
 
 //alterando propriedade do obj
-people.nome = 'Luma Loka'
+people.nome = 'Luma Loka';
 
 // deletando propriedade do obj, delete retorna valor true caso apague 
 // uma propriedade ou tente apagar uma propriedade que já existe
 console.log(delete people.profissao);
 
+//alterando obj com espalhamento
+const peopleWithHouse = {...people, ap: true };
+
+//atribuindo a uma variavel de mesmo nome uma propriedade do obj
+const { nome } = peopleWithHouse;
+
+//usando espalhamento para imprimir só dados especificos do obj
 console.log(people);
+console.log(peopleWithHouse);
+console.log(nome);
+
+function printData(data){
+    const {nome, telefone} = people;
+    console.log(nome, telefone);
+}
+
+printData(people)
+
+function printDataS({nome, telefone, ap}){
+    console.log(nome, telefone, ap);
+};
+
+printDataS(people);
+printDataS(peopleWithHouse);
